f888cf0b0d0e25518c21c6defa018bc0bedd2cdd
mara <ms.marafinkel@gmail.com>
2021-08-16 16:09:09 -0700

    Add map component

---
+++ frontend/src/components/map/map.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Link } from 'react-router-dom';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 37.783964,
+      lng: -122.2446203
+    },
+    zoom: 11
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: window.googleAPIKey }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent
+            lat={37.783964}
+            lng={-122.2446203}
+            text={<Link to="/dispensaries/1"><img alt="Harborside" title="Harborside" className="dispensary-icon" src="images/icons/dispensary.png" /></Link>}
+          />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
+
