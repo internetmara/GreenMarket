9e626f78bde63c6ebbaf371406a169790f85afb6
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-16 11:12:11 -0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    products: {
+        type: Array,
+        required: false
+    },
+    services: {
+        type: Array,
+        required: false
+    },
+    reviews: {
+        type: Array,
+        required: false
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('users', UserSchema);
+module.exports = User;
