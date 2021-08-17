12d6bc57760ec62647b46ee95f43cba31c32ee58
Christopher DellAcqua <dellacquachristopher@gmail.com>
2021-08-16 22:18:58 -0700

    Created post request for product

--- routes/api/products.js
+++ routes/api/products.js
@@ -9,4 +9,24 @@ const validateProductInput = require('../../validation/products')
 
 router.get("/test", (req, res) => res.json({ msg: "This is the product route" }));
 
+router.post('/',
+    passport.authenticate('jwt', {session: false}),
+    (req, res) => {
+        const {errors, isValid} = validateProductInput(req.body);
+
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newProduct = new Product({
+            category: req.body.category,
+            rate: req.body.rate,
+            description: req.body.description,
+            address: req.body.address,
+            user: req.user.id
+        })
+        newProduct.save().then(product => res.json(product));
+    });
+
+
 module.exports = router;
