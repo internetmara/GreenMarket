924e7c894559b0cbd105aa0d523adf0eef804f5c
Veronika <veronika.pilipenko@gmail.com>
2021-08-17 17:39:02 -0400

    added user seeds - aobout to add products and services

---
+++ seeds.js
@@ -0,0 +1,157 @@
+//users/ entitities 
+[
+    {
+        "name": "Mara",
+        "username": "maralovescoffee",
+        "email": "mara@gmail.com",
+        "address": "100 Main St., San Francisco, CA 94111",
+        "avatar": 'marasface.jpg',
+        "products": [200, 201],
+        "services": [300],
+    },
+    {
+        "name": "Veronika",
+        "username": "veronika",
+        "email": "veronika@gmail.com",
+        "address": "200 Main St., San Francisco, CA 94111",
+        "avatar": 'mishka.jpg',
+        "products": [300, 331],
+        "services": [20],
+    },
+    {
+        "name": "Erik",
+        "username": "erikingebretson",
+        "email": "erikiswholesome@gmail.com",
+        "address": "300 Main St., San Francisco, CA 94111",
+        "avatar": 'eriklovestocode.jpg',
+        "products": [400, 231],
+        "services": [70],
+    },
+    {
+        "name": "Chris",
+        "username": "delaquauniversity",
+        "email": "chris@chris.com",
+        "address": "400 Main St., San Francisco, CA 94111",
+        "avatar": 'chris.jpg',
+        "products": [250, 272],
+        "services": [60],
+    },
+    {
+        "name": "Mike",
+        "username": "michaelm",
+        "email": "kittymike@gmail.com",
+        "address": "500 Main St., San Francisco, CA 94111",
+        "avatar": 'meow.jpg',
+        "products": [231, 293],
+        "services": [33],
+    },
+    {
+        "name": "Zamin",
+        "username": "zaminlovespinterest",
+        "email": "zamin@gmail.com",
+        "address": "600 Main St., San Francisco, CA 94111",
+        "avatar": 'zamin.jpg',
+        "products": [2, 777],
+        "services": [300],
+    },
+    {
+        "name": "Nicha",
+        "username": "nichaaa",
+        "email": "nicha@gmail.com",
+        "address": "700 Main St., San Francisco, CA 94111",
+        "avatar": 'nicha',
+        "products": [384, 201],
+        "services": [198],
+    },
+    {
+        "name": "Annie",
+        "username": "annie",
+        "email": "annie@gmail.com",
+        "address": "800 Main St., San Francisco, CA 94111",
+        "avatar": 'annie.jpg',
+        "products": [123, 498],
+        "services": [389],
+    },
+    {
+        "name": "Paul",
+        "username": "paulkim",
+        "email": "paul@gmail.com",
+        "address": "900 Main St., San Francisco, CA 94111",
+        "avatar": 'paul_kim',
+        "products": [44, 231],
+        "services": [331],
+    }
+]
+
+//product seeds
+
+// [
+//     {
+//         "name": "tomatos",
+//         "price": 10,
+//         "category": "vegetables",
+//         "size": "5 tomatoes"
+//     },
+//     {
+//         "name": "bread",
+//         "id": ""
+//         "price": "bread" ,
+//         "category": "food",
+//         "size": "1 loaf",
+//         "subCategory": 'baking',
+//         "rate": 'static',
+//         "description" : 'Fresh baked sourdough bread everyday! $5 a loaf!',
+//         "address": '825 Battery St, San Francisco, CA 94111',
+//         "avatar": '',
+//         "pictures": [],
+//         "reviews": []
+//     },
+//     {
+//         "name": "blueberries",
+//         "price": 5,
+//         "category": "fruit",
+//         "size": "20 blueberries"
+//     },
+//     {
+//         "name": "muffin",
+//         "price": 3,
+//         "category": "baked goods",
+//         "size": "1 muffin"
+//     },
+//     {
+//         "name": "basil",
+//         "price": 2,
+//         "category": "herbs",
+//         "size": "1 bunch of basil"
+//     },
+//     {
+//         "name": "mint",
+//         "price": 3,
+//         "category": "herbs",
+//         "size": "1 bunch of mint"
+//     },
+//     {
+//         "name": "strawberries",
+//         "price": 20,
+//         "category": "fruit",
+//         "size": "5 pounds of strawberries"
+//     },
+//     {
+//         "name": "plumbing",
+//         "price": 30,
+//         "category": "plumbing",
+//         "size": "any kind of plumbing service that is needed"
+//     },
+//     {
+//         "name": "carpenter",
+//         "price": 45,
+//         "category": "carpentry",
+//         "size": "any kind of carpentry that is needed"
+//     },
+//     {
+//         "name": "avocados",
+//         "price": 12,
+//         "category": "fruit",
+//         "size": "6 avocados"
+//     }
+// ]
