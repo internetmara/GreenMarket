35dc372f701c5257eb5f270349d70a79a3bf8c49
mara <ms.marafinkel@gmail.com>
2021-08-18 00:09:04 -0700

    Configure app for heroku

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+// keys_dev.js
+module.exports = {
+  mongoURI: 'mongodb+srv://dev:lwGd9JdoiSrZwRB6@cluster0.sy1iy.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  secretOrKey: 'secretbBTIbtfgo4wbt43h'
+}
