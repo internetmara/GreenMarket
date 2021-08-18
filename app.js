ca258d42dd8fb6aa1f069129289b7462c0675468
mara <ms.marafinkel@gmail.com>
2021-08-17 22:06:27 -0700

    Heroku keys 2

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const db = require('./config/keys').mongoURI;
 const mongoose = require('mongoose');
 const users = require("./routes/api/users");
 const products = require('./routes/api/products')
@@ -11,12 +10,13 @@ const passport = require('passport');
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
-  app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  })
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
 }
 
+const db = require('./config/keys').mongoURI;
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to MongoDB successfully"))
