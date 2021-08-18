842b1cc2776f76e33d61c6b917d73dff7ec330f1
mara <ms.marafinkel@gmail.com>
2021-08-17 22:15:43 -0700

    Heroku test

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,20 +4,8 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
-
-
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
-
-const path = require('path');
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
-}
-
 const keys = require('../../config/keys');
 // router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
