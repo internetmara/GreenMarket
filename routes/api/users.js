8d0bfc6ba1898b2598fa1fd3e133748560755852
mara <ms.marafinkel@gmail.com>
2021-08-17 22:52:33 -0700

    Configure app for heroku

--- routes/api/users.js
+++ routes/api/users.js
@@ -8,14 +8,13 @@ const passport = require('passport');
 const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 const path = require('path');
-
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('frontend/build'));
     app.get('/', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
 }
-const keys = require('../../config/keys');
+// const keys = require('../../config/keys');
 // router.get("/test", (req, res) => res.json({ msg: "This is the users route" }));
 
 router.get('/current', passport.authenticate('jwt', { session: false }), (req, res) => {
