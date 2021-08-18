613565280ba94bc714c608882041ac92658854b8
mara <ms.marafinkel@gmail.com>
2021-08-18 10:33:52 -0700

    update google maps key

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -2,5 +2,5 @@
 module.exports = {
   mongoURI: process.env.MONGO_URI,
   secretOrKey: process.env.SECRET_OR_KEY,
-  googleMapsKey: process.env.GOOGLE_MAPS_KEYS
+  googleMapsKey: process.env.GOOGLE_MAPS_KEY
 }
