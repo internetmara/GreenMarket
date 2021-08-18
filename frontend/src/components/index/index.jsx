f47c861721d317d1af1440a61f2f3fbfcb5a88d6
erikingebretson <erikingebretson@gmail.com>
2021-08-18 00:06:05 -0400

    update frontend

--- frontend/src/components/index/index.jsx
+++ frontend/src/components/index/index.jsx
@@ -6,6 +6,13 @@ class IndexComponent extends React.Component {
         super(props)
     }
 
+// ** need to **
+// index all products and services
+    // do this through simple get axios calls
+// show each individual item
+    // iterate through all items to get these && stored in frontend state
+// show each individual item's seller
+    // send a axios get request for product owner info - send product owner's id
 
 
 
