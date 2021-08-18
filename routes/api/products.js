d70be11c34459b6d0d3888312ef995ed2b2cf0b1
mara <ms.marafinkel@gmail.com>
2021-08-17 22:57:05 -0700

    Configure app for heroku

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const app = express();
 const router = express.Router();
 const Product = require('../../models/Product')
 const User = require('../../models/User')
