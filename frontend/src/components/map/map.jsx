323189d940a01b3fc7d379817486ee1bb2174e7f
erikingebretson <erikingebretson@gmail.com>
2021-08-19 13:03:34 -0700

    broke placeItems

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -13,7 +13,7 @@ class SimpleMap extends Component {
     super(props)
   };
     
-  populateItems() {
+  populateItems = async () => {
     Geocode.setApiKey(key);
     Geocode.setLanguage("en");
     Geocode.setRegion("us");
@@ -22,42 +22,63 @@ class SimpleMap extends Component {
 
     let allCoords = [];
     let items = Object.values(this.props.products).concat(Object.values(this.props.services))
-    items.forEach( async (ele, idx) => {
-      const res = await Geocode.fromAddress(ele.address)
+    // items.forEach( async (ele, idx) => {
+    //   const res = await Geocode.fromAddress(ele.address)
+    //   const { lat, lng } = res.results[0].geometry.location;
+
+    //   newarr = await allCoords.push([lat, lng])
+    //   // // console.log('coords' + allCoords.length)
+    //   // // console.log('items' + items.length)
+      
+    //   // if (allCoords.length === items.length - 1) {
+    //     // for (let i=0; i < items.length; i++ ) {
+    //       //   // console.log(allCoords[i][1]) 
+    //       //    this.createItem( allCoords[i][0], allCoords[i][1])
+    //     // }
+    //   // }
+    // })
+
+    // for await (let i = 0; i < items.length; i++) {
+    for (const item of items) {
+      // console.log(item)
+      const res = await Geocode.fromAddress(item.address)
+      console.log(res)
       const { lat, lng } = res.results[0].geometry.location;
-      // ATTEMPT
       allCoords.push([lat, lng])
-      // ATTEMPT
-    })
-    
-    // ATTEMPT
-    console.log(allCoords)
+    }
+
+    console.log(allCoords[0])
+    // console.log(allCoords instanceof Array)
+    // console.log(allCoords.length)
+
     return items.map( (ele, idx) => {
-      return this.createItem(allItems[idx][0], allItems[idx][1], ele)
+      // console.log(allCoords[idx])
+      if (allCoords[idx] === undefined) return null; 
+      return this.createItem( allCoords[idx][0], allCoords[idx][1], ele)
     })
-    // ATTEMPT
   }
 
   createItem = (lat, lng, item) => {
-    console.log(lat, lng)
+    // console.log(lat, lng)
     return (
       <div>
         <AnyReactComponent
-            lat={lat}
-            lng={lng}
-            // lat = { 39.9264719 }
-            // lng = { -105.0424311 }
+            // lat={lat}
+            // lng={lng}
+            lat = { 39.9264719 }
+            lng = { -105.0424311 }
               //  text={< Link to={`/${item.category}/${item.id}`} > <img alt="N/A" title="N/A" className="GM-icon" src="/logo192.png" /></Link >}
             text={< Link to="/" > <img alt="N/A" title="N/A" className="GM-icon" src="/logo192.png" /></Link >}
         />
       </div>
     )
   }
-  // lat = { 39.9264719 }
-  // lng = { -105.0424311 }
 
   render() {
-    // console.log(this.props)
+    
+    if (this.props.services instanceof Object && Object.values(this.props.services).length === 0 || this.props.products instanceof Object && Object.values(this.props.products).length === 0) return null;
+    // if (this.props.userLat === 0 || this.props.userLng === 0) return null;
+    // debugger
     return (
       // Important! Always set the container height explicitly
 
