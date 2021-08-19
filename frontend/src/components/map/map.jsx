c8411037b53c579c06f86225efa7df838bdadf85
erikingebretson <erikingebretson@gmail.com>
2021-08-19 05:45:34 -0400

    try await in geocode

--- frontend/src/components/map/map.jsx
+++ frontend/src/components/map/map.jsx
@@ -10,40 +10,51 @@ const AnyReactComponent = ({ text }) => <div>{text}</div>;
 class SimpleMap extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      lat: 0,
+      lng: 0
+    };
     this.lat = 0;
     this.lng = 0;
   };
     
     populateItems() {
-      
       let items = Object.values(this.props.products).concat(Object.values(this.props.services))
-      return items.map(item => {
-        Geocode.setApiKey(key);
-        Geocode.setLanguage("en");
-        Geocode.setRegion("us");
-        // Geocode.enableDebug();
-        Geocode.setLocationType("APPROXIMATE");
+      let latit = 0;
+      let lngit = 0;
+      Geocode.setApiKey(key);
+      Geocode.setLanguage("en");
+      Geocode.setRegion("us");
+      // Geocode.enableDebug();
+      Geocode.setLocationType("APPROXIMATE");
+      // return items.map(item => {
+        for (let i=0; i < items.length; i++) {
+         await Geocode.fromAddress(items[i].address).then( 
+          res => {
+            this.lat = res.results[0].geometry.location.lat
+            this.lng = res.results[0].geometry.location.lng
+            console.log(this.lat, this.lng)
+          }
+          ).catch(err => {console.log(err)})
+        console.log(this.lat, this.lng)
+        this.createItem(this.lat, this.lng)
+        }
         
-        Geocode.fromAddress(item.address).then( 
-        res => {
-          console.log(res)
-          // console.log(res.results[0].geometry.location.lat)
-          // ({ lat, lng } = res.results[0].geometry.location)
-          this.lat = res.results[0].geometry.location.lat
-          this.lng = res.results[0].geometry.location.lng
-        })
-        console.log(`item${item.name}` + this.lat, this.lng)
-      return <div>
-        < AnyReactComponent
-          lat={this.lat}
-          lng={this.lng}
-          text={< Link to="/" > <img alt="N/A" title="N/A" className="GM-icon" src="/logo192.png" /></Link >}
-        />
-      </div>
-    })
+    // })
   }
 
+  createItem(lat, lng, item) {
+    return <div>
+         < AnyReactComponent
+        lat={39.782267}
+        lng={-104.8919341}
+           text={< Link to="/" > <img alt="N/A" title="N/A" className="GM-icon" src="/logo192.png" /></Link >}
+         />
+       </div>
+  }
+// 39.782267, -104.8919341
   render() {
+    console.log(this.props)
     return (
       // Important! Always set the container height explicitly
 
@@ -54,6 +65,7 @@ class SimpleMap extends Component {
           zoom={11}
           >
             {this.populateItems()}
+            {/* {this.createItem()} */}
         </GoogleMapReact>
       </div>
     );
