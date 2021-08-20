0a68de79271c4ceeb7aa073c5d64895322675a45
mara <ms.marafinkel@gmail.com>
2021-08-19 20:01:27 -0700

    Restyle

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -7,16 +7,18 @@ class Splash extends React.Component {
     render() {
         return (
              <div className="home-container">
+                 <div className="home-links">
+                    <Link to="/products/new"><h2 className="go-to-the-map">List a product</h2></Link>
+                    <Link to="/services/new"><h2 className="go-to-the-map">Offer a Service</h2></Link>
+                    <Link to="/map"><h2 className="go-to-the-map">Map</h2></Link>
+                    <Link to="/user"><h2 className="go-to-the-map">Profile</h2></Link>
+                </div>
                  <img className="home-logo" src="/images/Green-Market.png"></img>
-                 <div className="login-signup2">
+                 {/* <div className="login-signup2">
                     <button className="home-page-button2"><Link to="/login">Log in</Link></button>
                     &nbsp;
                     <button className="home-page-button2"><Link to="/signup">Sign up</Link></button>
-                </div>
-                <Link to="/map"><h2 className="go-to-the-map">Go To The Map!</h2></Link>
-                <Link to="/products/new"><h2 className="go-to-the-map">Go To The Product Uploader!</h2></Link>
-                <Link to="/services/new"><h2 className="go-to-the-map">Go To The Service Uploader!</h2></Link>
-                <Link to="/user"><h2 className="go-to-the-map">Go To Your Show Page!</h2></Link>
+                </div> */}
             </div>
         )
     }
