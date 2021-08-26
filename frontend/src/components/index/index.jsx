7e31f2a9df2c919ffd211a1f5e9ba5097a9a3a14
erikingebretson <erikingebretson@gmail.com>
2021-08-26 09:19:08 -0700

    create item popup modal

--- frontend/src/components/index/index.jsx
+++ frontend/src/components/index/index.jsx
@@ -2,10 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import ItemList  from './item_list';
 import SimpleMap from '../map/map'
-import Geocode from "react-geocode";
 import '../styling/reset.css';
-const key = require("../../config/keys").googleMapsKey;
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class IndexComponent extends React.Component {
     constructor(props) {
@@ -18,12 +15,13 @@ class IndexComponent extends React.Component {
     }
 
     render() {
-        // console.log(this.props.user)
         return (
-            <div>
+            <div className="index-root">
                 <Link to="/"><h2 className="map-header">Local Goods & Services</h2></Link>
-                <ItemList products={this.props.products} services={this.props.services} />
-                <SimpleMap products={this.props.products} services={this.props.services} user={this.props.user} coordsLat={this.props.user.coordsLat} coordsLng={this.props.user.coordsLng} />
+                <div className="index-items">
+                    <ItemList products={this.props.products} services={this.props.services} />
+                    <SimpleMap products={this.props.products} services={this.props.services} user={this.props.user} coordsLat={this.props.user.coordsLat} coordsLng={this.props.user.coordsLng} />
+                </div>
             </div>
         )
     }
--- frontend/src/components/index/item_list.jsx
+++ frontend/src/components/index/item_list.jsx
@@ -6,34 +6,53 @@ class ItemList extends React.Component {
         super(props)
     }
 
+    showItem(id) {
+        // let bg = document.getElementsByClassName('item-box')
+        // console.log(bg)
+        // Object.values(bg)[0].style.display = 'flex'
+        let el = document.getElementsByClassName(id)
+        Object.values(el)[0].style.display = 'flex'
+    }
+
+    hideItem(id) {
+        let el = document.getElementsByClassName(id)
+        Object.values(el)[0].style.display = 'none'
+    }
+
     listAllItems() {
         let items = Object.values(this.props.products).concat(Object.values(this.props.services))
-        // console.log(items.length)
+        
         if (items.length === 0) return null;
         return items.map( item => {
-         return <div key={item._id} className="item">
-                    {/* {console.log(this.props.products[item._id])} */}
-                    {
-                        this.props.products[item._id] !== undefined ? 
+                return <div key={item._id} className="item">
+                    {/* {this.props.products[item._id] !== undefined ? 
                         <Link to={`/product/${item._id}`} ><p>{item.name}</p></Link> :
-                        <Link to={`/service/${item._id}`} ><p>{item.name}</p></Link>
-                    }
+                        <Link to={`/service/${item._id}`} ><p>{item.name}</p></Link>} */}
+                    <p onClick={() => this.showItem(`item-show-${item._id}`)} >{item.name}</p>
                     <p>{item.category}</p>
                     <p>{item.rate}</p>
+                        <div className={`item-show-${item._id}`} style={{ display: "none" }}>
+                            <div className="item-show-root">
+                                {/* <img src="{}"></img> */}
+                                <p>Name: {item.name}</p>
+                                <p>Category: {item.category}</p>
+                                <p>Rate: {item.rate}</p>
+                                <p>Description: {item.description}</p>
+                                <p>Address: {item.address}</p>
+                                <p onClick={() => this.hideItem(`item-show-${item._id}`)} >X</p>
+                            </div>
+                        </div>
+                    
                 </div>
         })
-
     }
 
     render() {
         
         return (
-            <div>
-                <h2>ITEMSLZT</h2>
                 <div className="items-index">
                     {this.listAllItems()}
                 </div>
-            </div>
         )
     }
 }
