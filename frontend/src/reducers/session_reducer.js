bae9483f62dc4d8db97b420097b6c7489a4e91e1
erikingebretson <erikingebretson@gmail.com>
2021-08-26 12:40:37 -0700

    fix cat filter on index

--- frontend/src/reducers/session_reducer.js
+++ frontend/src/reducers/session_reducer.js
@@ -8,7 +8,6 @@ const initialState = {
 export default function(state=initialState, action) {
   switch (action.type) {
     case RECEIVE_CURRENT_USER:
-      console.log(action)
       return {
         ...state,
         isAuthenticated: !!action.currentUser,
