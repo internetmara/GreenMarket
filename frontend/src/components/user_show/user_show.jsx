5bf6351befb13d5b4106d5cc1ed1904ab76ed29d
mara <ms.marafinkel@gmail.com>
2021-08-30 16:49:16 -0700

    Merge

--- frontend/src/components/user_show/user_show.jsx
+++ frontend/src/components/user_show/user_show.jsx
@@ -11,7 +11,6 @@ class userShow extends React.Component {
         this.props.getServices();
         this.props.getProducts();
         this.props.getUser(this.props.user.id);
-        console.log("hi!")
     }
 
     render() {
