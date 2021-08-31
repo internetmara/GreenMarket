a0f958ad2ed3091f2239efedebdb0b869599ee21
mara <ms.marafinkel@gmail.com>
2021-08-30 21:00:30 -0700

    Style about page

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -11,42 +11,42 @@ class About extends React.Component {
             <div className="creator-links">
               
                <div className="person">
-                <img src="/images/Chris.png" className="profile-pic" alt="Christopher Dell'Acqua"/>
+                <img src="/images/profilepics/Chris.png" className="profile-pic" alt="Christopher Dell'Acqua"/>
                 <h2 className="profile-name">Christopher Dell'Acqua</h2>
                 <div className="profile-links">
-                  <a href="https://github.com/cdellacqua123">Github</a>
-                  <a href="https://www.linkedin.com/in/christopher-dell-acqua-b6765995/">Linkedin</a>
-                  <a href="">Personal Site</a>
+                  <a href="https://github.com/cdellacqua123"><img className="social-pic" src="/images/socials/github.png"/></a>
+                  <a href="https://www.linkedin.com/in/christopher-dell-acqua-b6765995/"><img className="social-pic" src="/images/socials/linkedin.png"/></a>
+                  <a href=""><img className="social-pic" src="/images/socials/userpic.png"/></a>
                 </div>
               </div>
 
               <div className="person">
-                <img src="/images/Erik.png" className="profile-pic" alt="Erik Ingebretson"/>
+                <img src="/images/profilepics/Erik.png" className="profile-pic" alt="Erik Ingebretson"/>
                 <h2 className="profile-name">Erik Ingebretson</h2>
                 <div className="profile-links">
-                  <a href="https://github.com/erikingebretson">Github</a>
-                  <a href="https://www.linkedin.com/in/erik-ingebretson/">Linkedin</a>
-                  <a href="">Personal Site</a>
+                  <a href="https://github.com/erikingebretson"><img className="social-pic" src="/images/socials/github.png"/></a>
+                  <a href="https://www.linkedin.com/in/erik-ingebretson/"><img className="social-pic" src="/images/socials/linkedin.png"/></a>
+                  <a href=""><img className="social-pic" src="/images/socials/userpic.png"/></a>
                 </div>
               </div>
 
                <div className="person">
-                <img src="/images/Mara.jpeg" className="profile-pic" alt="Mara Finkel"/>
+                <img src="/images/profilepics/Mara.jpeg" className="profile-pic" alt="Mara Finkel"/>
                 <h2 className="profile-name">Mara Finkel</h2>
                 <div className="profile-links">
-                  <a href="https://github.com/internetmara">Github</a>
-                  <a href="https://www.linkedin.com/in/marafinkel">Linkedin</a>
-                  <a href="">Personal Site</a>
+                  <a href="https://github.com/internetmara"><img className="social-pic" src="/images/socials/github.png"/></a>
+                  <a href="https://www.linkedin.com/in/marafinkel"><img className="social-pic" src="/images/socials/linkedin.png"/></a>
+                  <a href=""><img className="social-pic" src="/images/socials/userpic.png"/></a>
                 </div>
               </div>
 
                <div className="person">
-                <img src="/images/Veronika.png" className="profile-pic" alt="Veronika Pilipenko"/>
+                <img src="/images/profilepics/Veronika.png" className="profile-pic" alt="Veronika Pilipenko"/>
                 <h2 className="profile-name">Veronika Pilipenko</h2>
                 <div className="profile-links">
-                  <a href="https://github.com/VPilipenko334">Github</a>
-                  <a href="https://www.linkedin.com/in/veronikapilipenko/">Linkedin</a>
-                  <a href="">Personal Site</a>
+                  <a href="https://github.com/VPilipenko334"><img className="social-pic" src="/images/socials/github.png"/></a>
+                  <a href="https://www.linkedin.com/in/veronikapilipenko/"><img className="social-pic" src="/images/socials/linkedin.png"/></a>
+                  <a href=""><img className="social-pic" src="/images/socials/userpic.png"/></a>
                 </div>
               </div>
             </div>
