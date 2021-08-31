9bcb322eb815bf749c6df95c9dee9df8890b6cac
Veronika <veronika.pilipenko@gmail.com>
2021-08-30 23:02:27 -0400

    final commit of the night

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -27,26 +27,28 @@ class Splash extends React.Component {
                          <div className="splash-first-container2"> 
                                 <div className="environment-container1">
                                     <img src="/images/1.png" className="environment-container2-image"/>
-                                        <h2>Our Mission</h2>
-                                    hello my name is veronika hi 
+                                        <h2>Who are we?</h2>
+                                        <br/>
+                                    Green Market is a sustainanble, community-driven, online service where neighbors can trade goods and services.
                                 </div>
 
                         <div className="environment-container2"><img src="/images/2.png" className="environment-container2-image"/>
-                                        <h2>It's Important!</h2>
-                                        hi
-        
+                                        <h2>Our Mission</h2>
+                                            <br/>
+                                     Our mission is to help local relationships form to build a more connected world. Exchanging goods and services will do just that.
                                 </div>
 
                         <div className="environment-container3"><img src="/images/3.png" className="environment-container2-image"/>
-                                        <h2>The Future</h2>
-                                        hallo
+                                        <h2>Our Vision</h2>
+                                        <br/>
+                                    In an increasingly disconnected world, Green Market is here to help neighbors connect sustainably.
                                 </div>
                                 {/* <div className="environment-container"><img src="/images/4.png"/> </div> */}
                         </div>
 
                         <br/>
                             <div className="splash-first-container">
-                                <p>api will potentially go here</p>
+                                <p>API will potentially go here</p>
                             </div>
                 </div>
         )
