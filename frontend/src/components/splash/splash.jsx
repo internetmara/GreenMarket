2af06ff4fc00992830b8b1a123a3f36bc5888153
mara <ms.marafinkel@gmail.com>
2021-09-01 14:59:39 -0700

    Style home container

--- frontend/src/components/splash/splash.jsx
+++ frontend/src/components/splash/splash.jsx
@@ -10,39 +10,34 @@ class Splash extends React.Component {
             <div className="home-container">
                  <img className="home-logo" src="/images/Green-Market.png" alt="Logo"></img>
                 {/* <img src="/images/4.png"/> */}
-
-                    <div className="splash-first-container">
+                    {/* <div className="splash-first-container">
                         <p>hello I am greeting container ~wave~</p>
+                    </div> */}
+                    {/* <br/> */}
+                <div className="splash-first-container2"> 
+                    <div className="environment-container1">
+                        <img src="/images/1.png" className="environment-container2-image"/>
+                        <h2>Who are we?</h2>
+                        <br/>
+                        Green Market is a sustainanble, community-driven, online service where neighbors can trade goods and services.
                     </div>
-
+                    <div className="environment-container2">
+                        <img src="/images/2.png" className="environment-container2-image"/>
+                        <h2>Our Mission</h2>
                         <br/>
-
-                         <div className="splash-first-container2"> 
-                                <div className="environment-container1">
-                                    <img src="/images/1.png" className="environment-container2-image"/>
-                                        <h2>Who are we?</h2>
-                                        <br/>
-                                    Green Market is a sustainanble, community-driven, online service where neighbors can trade goods and services.
-                                </div>
-
-                        <div className="environment-container2"><img src="/images/2.png" className="environment-container2-image"/>
-                                        <h2>Our Mission</h2>
-                                            <br/>
-                                     Our mission is to help local relationships form to build a more connected world. Exchanging goods and services will do just that.
-                                </div>
-
-                        <div className="environment-container3"><img src="/images/3.png" className="environment-container2-image"/>
-                                        <h2>Our Vision</h2>
-                                        <br/>
-                                    In an increasingly disconnected world, Green Market is here to help neighbors connect sustainably.
-                                </div>
-                        </div>
-
+                        Our mission is to help local relationships form to build a more connected world. Exchanging goods and services will do just that.
+                    </div>
+                    <div className="environment-container3"><img src="/images/3.png" className="environment-container2-image"/>
+                        <h2>Our Vision</h2>
                         <br/>
-                            <div className="splash-first-container">
-                                <p>API will potentially go here</p>
-                            </div>
+                        In an increasingly disconnected world, Green Market is here to help neighbors connect sustainably.
+                    </div>
                 </div>
+                    {/* <br/> */}
+                        {/* <div className="splash-first-container">
+                            <p>API will potentially go here</p>
+                        </div> */}
+            </div>
         )
     }
 }
